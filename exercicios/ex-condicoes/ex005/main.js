71fe550d68cb143afbb50031bf8e2450b0b832f0
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-01-09 15:57:42 -0300

    melhorias nos códigos

--- exercicios/ex-condicoes/ex005/main.js
+++ exercicios/ex-condicoes/ex005/main.js
@@ -7,13 +7,13 @@ function carregar(){
     document.getElementById('p-horas').innerHTML = horaFormatada
 
     if(hora >= 0 && hora < 12) {
-        imagem.style.backgroundImage = 'url(imagens/manha.jpg)'
+        imagem.src = 'imagens/manha.png'
         document.body.style.backgroundColor = 'rgb(236, 218, 167)'
     } else if(hora >= 12 && hora < 18) {
-        imagem.style.backgroundImage = 'url(imagens/tarde.jpg)'
+        imagem.src = 'imagens/tarde.png'
         document.body.style.backgroundColor = 'rgb(214, 124, 40)'
     } else {
-        imagem.style.backgroundImage = 'url(imagens/noite.jpg)'
+        imagem.src = 'imagens/noite.png'
         document.body.style.backgroundColor = 'rgb(70, 70, 70)'
     }
 }
