46e109bd513d4a2f6e9034df7ca341cb0b2e9685
Thays Oliveira <thaysoliveira14@hotmail.com>
2024-01-20 18:02:01 -0300

    correções

--- exercicios/ex-final/main.js
+++ exercicios/ex-final/main.js
@@ -9,17 +9,24 @@ function adicionar() {
     // Obtém os elementos HTML necessários
     let conteiner = document.getElementById('conteiner')
     let btFinalizar = document.getElementById('fin')
+    let resultado = document.getElementById('resultado')
 
     if(num == '' || num < 1 || num > 100 ) {
-        alert("Por favor, digite um número entre 1 e 100.")
+        resultado.style.color = 'red'
+        resultado.style.fontSize = '0.8em'
+        resultado.style.textAlign = 'center'
+        resultado.textContent = "Por favor, digite um número entre 1 e 100."
     } else {
-        let resultado = document.getElementById('resultado')
+        
         // Limpa o conteúdo da div#resultado
-        resultado.innerHTML = ''
+        resultado.textContent = ''
 
         // Verifica se o número já está no array
         if(numeros.includes(Number(num))){
-            alert("Este número já foi adicionado!")
+            resultado.style.color = 'red'
+            resultado.style.fontSize = '0.8em'
+            resultado.style.textAlign = 'center'
+            resultado.textContent = 'Este número já foi adicionado! Por favor, digite outro.'
         } else {
             // Adiciona o número digitado ao array
             numeros.push(Number(num))
@@ -34,7 +41,6 @@ function adicionar() {
             numInput.value = ''
 
             // Exibe o botão "Finalizar"
-            let btFinalizar = document.getElementById('fin')
             btFinalizar.style.display = 'inline-block'
         }
         
@@ -53,11 +59,11 @@ function finalizar() {
     // Limpa o conteúdo da div#resultado
     resultado.innerHTML = ''
     // Exibe as informações sobre os números digitados
-    resultado.innerHTML += `• Ao todo temos ${numeros.length} números cadastrados.<br>`
-    resultado.innerHTML += `• O menor valor informado foi ${numeros[0]}.<br>`
-    resultado.innerHTML += `• O maior valor informado foi ${numMaior}.<br>`
-    resultado.innerHTML += `• A soma dos valores digitados é ${soma}.<br>`
-    resultado.innerHTML += `• A média dos valores digitados é ${(soma / numeros.length).toFixed(2)}.`
+    resultado.innerHTML = `• Ao todo temos ${numeros.length} números cadastrados.<br>
+    • O menor valor informado foi ${numeros[0]}.<br>
+    • O maior valor informado foi ${numMaior}.<br>
+    • A soma dos valores digitados é ${soma}.<br>
+    • A média dos valores digitados é ${(soma / numeros.length).toFixed(2)}`
 }
 
 
